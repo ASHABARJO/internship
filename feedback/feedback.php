3a706185381da41e67f83a05fcdfe04b3d3f4159
ASHA BARJO <89405319+ASHABARJO@users.noreply.github.com>
2024-05-10 18:04:31 +0530

    Create feedback.php

---
+++ feedback/feedback.php
@@ -0,0 +1,251 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <title>Feedback</title>
+    <style>
+        .card-1{
+            min-height: 500px;
+        }
+        b{
+            font-size: 60px !important;
+            color:green;
+        }
+        b:hover{
+            color:blue;
+        }
+        i{
+            font-size:30px !important;
+            color:#FFD700;
+        }
+    </style>
+</head>
+<body>
+
+
+<div id="carouselExampleIndicators" class="carousel slide">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+        <!-- 1 slide -->
+    <div class="container mt-5">
+        <div class="card card-1">
+          <div class="container">
+          <div class="row mt-5">
+                <div class="col-lg-4 col-sm-4 col-md-4 mt-5">
+                    <div class="card text-center shadow-lg">
+                        <div class="card-body">
+                        <b class="fa fa-address-book" aria-hidden="true"></b>
+                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Libero alias ipsa a hic natus magnam odit possimus repudiandae error nemo omnis distinctio aspernatur veniam eius consectetur autem, ipsam porro dolores sint! Tenetur, iste. Corrupti quos consequatur aliquam, non laborum dignissimos!</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-sm-4 col-md-4 mt-5">
+                <div class="card text-center shadow-lg">
+                        <div class="card-body">
+                        <b class="fa fa-address-card-o" aria-hidden="true"></b>
+                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Libero alias ipsa a hic natus magnam odit possimus repudiandae error nemo omnis distinctio aspernatur veniam eius consectetur autem, ipsam porro dolores sint! Tenetur, iste. Corrupti quos consequatur aliquam, non laborum dignissimos!</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-sm-4 col-md-4 mt-5">
+                <div class="card text-center shadow-lg">
+                        <div class="card-body">
+                        <b class="fa fa-user-circle" aria-hidden="true"></b>
+                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Libero alias ipsa a hic natus magnam odit possimus repudiandae error nemo omnis distinctio aspernatur veniam eius consectetur autem, ipsam porro dolores sint! Tenetur, iste. Corrupti quos consequatur aliquam, non laborum dignissimos!</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+          </div>
+
+          <div class="row justify-content-center mt-5">
+            <div class="col-lg-2 text-center">
+            <i class="fa fa-star" aria-hidden="true"></i>
+            <i class="fa fa-star" aria-hidden="true"></i>
+            <i class="fa fa-star" aria-hidden="true"></i>
+            <i class="fa fa-star" aria-hidden="true"></i>
+            <i class="fa fa-star-half" aria-hidden="true"></i>
+            </div>
+          </div>
+
+          <div class="row justify-content-center mt-5">
+            <div class="col-lg-8 text-center">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae harum illo soluta natus alias porro magni nemo cum ex suscipit!</p>
+            </div>
+          </div>
+        </div>
+    </div>
+    </div>
+    <div class="carousel-item">
+        <!-- 2 slide -->
+    <div class="container mt-5">
+        <div class="card card-1">
+          <div class="container">
+          <div class="row mt-5">
+                <div class="col-lg-4 col-sm-4 col-md-4 mt-5">
+                    <div class="card text-center shadow-lg">
+                        <div class="card-body">
+                        <b class="fa fa-address-book" aria-hidden="true"></b>
+                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Libero alias ipsa a hic natus magnam odit possimus repudiandae error nemo omnis distinctio aspernatur veniam eius consectetur autem, ipsam porro dolores sint! Tenetur, iste. Corrupti quos consequatur aliquam, non laborum dignissimos!</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-sm-4 col-md-4 mt-5">
+                <div class="card text-center shadow-lg">
+                        <div class="card-body">
+                        <b class="fa fa-address-card-o" aria-hidden="true"></b>
+                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Libero alias ipsa a hic natus magnam odit possimus repudiandae error nemo omnis distinctio aspernatur veniam eius consectetur autem, ipsam porro dolores sint! Tenetur, iste. Corrupti quos consequatur aliquam, non laborum dignissimos!</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-sm-4 col-md-4 mt-5">
+                <div class="card text-center shadow-lg">
+                        <div class="card-body">
+                        <b class="fa fa-user-circle" aria-hidden="true"></b>
+                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Libero alias ipsa a hic natus magnam odit possimus repudiandae error nemo omnis distinctio aspernatur veniam eius consectetur autem, ipsam porro dolores sint! Tenetur, iste. Corrupti quos consequatur aliquam, non laborum dignissimos!</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+          </div>
+
+          <div class="row justify-content-center mt-5">
+            <div class="col-lg-2 text-center">
+            <i class="fa fa-star" aria-hidden="true"></i>
+            <i class="fa fa-star" aria-hidden="true"></i>
+            <i class="fa fa-star" aria-hidden="true"></i>
+            <i class="fa fa-star-half" aria-hidden="true"></i>
+            </div>
+          </div>
+
+          <div class="row justify-content-center mt-5">
+            <div class="col-lg-8 text-center">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae harum illo soluta natus alias porro magni nemo cum ex suscipit!</p>
+            </div>
+          </div>
+        </div>
+    </div>
+    </div>
+    <!-- 3 slide -->
+    
+    <div class="carousel-item">
+        
+    <div class="container mt-5">
+        <div class="card card-1">
+          <div class="container">
+          <div class="row mt-5">
+                <div class="col-lg-4 col-sm-4 col-md-4 mt-5">
+                    <div class="card text-center shadow-lg">
+                        <div class="card-body">
+                        <b class="fa fa-address-book" aria-hidden="true"></b>
+                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Libero alias ipsa a hic natus magnam odit possimus repudiandae error nemo omnis distinctio aspernatur veniam eius consectetur autem, ipsam porro dolores sint! Tenetur, iste. Corrupti quos consequatur aliquam, non laborum dignissimos!</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-sm-4 col-md-4 mt-5">
+                <div class="card text-center shadow-lg">
+                        <div class="card-body">
+                        <b class="fa fa-address-card-o" aria-hidden="true"></b>
+                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Libero alias ipsa a hic natus magnam odit possimus repudiandae error nemo omnis distinctio aspernatur veniam eius consectetur autem, ipsam porro dolores sint! Tenetur, iste. Corrupti quos consequatur aliquam, non laborum dignissimos!</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-sm-4 col-md-4 mt-5">
+                <div class="card text-center shadow-lg">
+                        <div class="card-body">
+                        <b class="fa fa-user-circle" aria-hidden="true"></b>
+                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Libero alias ipsa a hic natus magnam odit possimus repudiandae error nemo omnis distinctio aspernatur veniam eius consectetur autem, ipsam porro dolores sint! Tenetur, iste. Corrupti quos consequatur aliquam, non laborum dignissimos!</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+          </div>
+
+          <div class="row justify-content-center mt-5">
+            <div class="col-lg-2 text-center">
+            <i class="fa fa-star" aria-hidden="true"></i>
+            <i class="fa fa-star" aria-hidden="true"></i>
+            <i class="fa fa-star" aria-hidden="true"></i>
+            </div>
+          </div>
+
+          <div class="row justify-content-center mt-5">
+            <div class="col-lg-8 text-center">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae harum illo soluta natus alias porro magni nemo cum ex suscipit!</p>
+            </div>
+          </div>
+        </div>
+    </div>
+    </div>
+    
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+
+
+    <!-- <div class="container mt-5">
+        <div class="card card-1">
+          <div class="container">
+          <div class="row mt-5">
+                <div class="col-lg-4">
+                    <div class="card text-center shadow-lg">
+                        <div class="card-body">
+                        <b class="fa fa-address-book" aria-hidden="true"></b>
+                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Libero alias ipsa a hic natus magnam odit possimus repudiandae error nemo omnis distinctio aspernatur veniam eius consectetur autem, ipsam porro dolores sint! Tenetur, iste. Corrupti quos consequatur aliquam, non laborum dignissimos!</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                <div class="card text-center shadow-lg">
+                        <div class="card-body">
+                        <b class="fa fa-address-card-o" aria-hidden="true"></b>
+                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Libero alias ipsa a hic natus magnam odit possimus repudiandae error nemo omnis distinctio aspernatur veniam eius consectetur autem, ipsam porro dolores sint! Tenetur, iste. Corrupti quos consequatur aliquam, non laborum dignissimos!</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                <div class="card text-center shadow-lg">
+                        <div class="card-body">
+                        <b class="fa fa-user-circle" aria-hidden="true"></b>
+                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Libero alias ipsa a hic natus magnam odit possimus repudiandae error nemo omnis distinctio aspernatur veniam eius consectetur autem, ipsam porro dolores sint! Tenetur, iste. Corrupti quos consequatur aliquam, non laborum dignissimos!</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+          </div>
+
+          <div class="row justify-content-center mt-5">
+            <div class="col-lg-2 text-center">
+            <i class="fa fa-star" aria-hidden="true"></i>
+            <i class="fa fa-star" aria-hidden="true"></i>
+            <i class="fa fa-star" aria-hidden="true"></i>
+            <i class="fa fa-star-half" aria-hidden="true"></i>
+            </div>
+          </div>
+
+          <div class="row justify-content-center mt-5">
+            <div class="col-lg-8 text-center">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae harum illo soluta natus alias porro magni nemo cum ex suscipit!</p>
+            </div>
+          </div>
+        </div>
+    </div> -->
+</body>
+</html>
